765089a22ab403839b07999de69438baa10160ee
nikhil <nikhilvs555@gmail.com>
2021-06-06 20:35:00 +0530

    added the version commad -v

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
-
-int main()
+#include<cstring>
+using namespace std;
+int main(int argc,char * argv[])
 {
-
+        //cout<<argv[0]<<argv[1];
+	if(argc>1)
+	if(strcmp(argv[1],"-v")==0)
+	{
+		cout<<"Version : "<<"1.0.0";
+		return 1;
+	}
 	cout<<"Hello World";
 }
 
